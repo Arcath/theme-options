0e36e44faa02fa3c4fc16d5f1f996637eb0e9b8a
Adam Laycock <adam@arcath.net>
2017-03-24 11:21:51 +0000

    initial commit

---
+++ Readme.markdown
@@ -0,0 +1,102 @@
+# Theme Options
+
+Theme Options Made Easy
+
+# Install
+
+Either install from composer `arcath/theme-options` or place `theme-options.php` in your theme and require it.
+
+# Usage
+
+Create a new ThemeOptions instance
+
+```php
+$themeOptions = new Arcath\ThemeOptions('slug');
+```
+
+In the `init` action define all your fields e.g.
+
+```php
+function slug_custom_fields(){
+  global $themeOptions;
+
+  $themeOptions->addThemeOptionCustomControl('logo_light',
+    array(
+      'type' => 'option',
+      'default' => 0
+    ),
+    array(
+      'section' => 'images',
+      'label' => __('Light Logo', 'slug'),
+      'description' => __('Light Logo used on darker backgrounds', 'glug'),
+    ),
+    'WP_Customize_Image_Control'
+  );
+
+  $themeOptions->addThemeOption('email',
+    array(
+      'capability' => 'edit_theme_options',
+      'type'       => 'option',
+      'default'    => 'foo@bar.com',
+    ),
+    array(
+      'label' => 'Email',
+      'section' => 'text',
+      'description' => __('Your Email')
+    )
+  );
+}
+
+add_action('init', 'slug_custom_fields');
+```
+
+In `customize_register` create your sections/panels and apply the theme options.
+
+```php
+function slug_apply_custom_fields($wp_customize){
+  global $themeOptions;
+
+  $wp_customize->add_panel('global', array(
+    'priority' => 1,
+    'capability'     => 'edit_theme_options',
+    'theme_supports' => '',
+    'title'          => __('Global Options', 'slug'),
+    'description'    => __('Global options that affect every page.', 'edit2017'),
+  ));
+
+  $wp_customize->add_section(
+    'images',
+    array(
+      'title' => 'Images',
+      'description' => 'Images used throughout the Theme',
+      'capability' => 'edit_theme_options',
+      'panel' => 'global'
+    )
+  );
+
+  $wp_customize->add_section(
+    'text',
+    array(
+      'title' => 'Text',
+      'description' => 'Text Fields',
+      'capability' => 'edit_theme_options',
+      'panel' => 'global'
+    )
+  );
+
+  $themeOptions->applyThemeOptions();
+}
+
+add_action('customize_register', 'slug_apply_custom_fields');
+```
+
+Then in your Theme you can pull any option using:
+
+```php
+<?php
+global $themeOptions;
+echo($themeOptions->themeOption('email'));
+?>
+```
+
+Theme Options handles returning the default if it has not been set in the post meta etc...
---
+++ theme-options.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Arcath;
+
+class ThemeOptions {
+  public $themeSlug;
+  private $themeOptions;
+
+  // Sets the theme slug on creation of the library
+  public function __construct($themeSlug = null){
+    $this->themeSlug = $themeSlug;
+    $this->themeOptions = array();
+  }
+
+  public function themeOption($name){
+    $options = (get_option($this->themeSlug.'_options')) ? get_option($this->themeSlug.'_options') : null;
+  	if(isset($options[$name])){
+  		$returnValue = apply_filters($this->themeSlug.'_options_'.$name, $options[$name]);
+  	}else{
+      $returnValue = apply_filters($this->themeSlug.'_options_'.$name, $this->themeOptions[$name][0]['default']);
+    }
+
+    $returnValue = apply_filters($this->themeSlug . '_' . $name, $returnValue);
+
+    return $returnValue;
+  }
+
+  // Add a theme option (standard control class)
+  public function addThemeOption($name, $settingArgs, $controlArgs){
+    global $wp_customize;
+
+    $controlArgs['settings'] = $this->optionName($name);
+
+    $this->themeOptions[$name] = array($settingArgs, $controlArgs, false);
+  }
+
+  // Add a theme option (custom control class)
+  public function addThemeOptionCustomControl($name, $settingArgs, $controlArgs, $controlClassString){
+    $controlArgs['settings'] = $this->optionName($name);
+
+    $this->themeOptions[$name] = array($settingArgs, $controlArgs, $controlClassString);
+  }
+
+  public function applyThemeOptions(){
+    global $wp_customize;
+
+    foreach($this->themeOptions as $name => $args){
+      if($args[2] == false){
+        $wp_customize->add_setting($this->optionName($name), $args[0]);
+        $wp_customize->add_control($this->optionName($name), $args[1]);
+      }else{
+        $wp_customize->add_setting($this->optionName($name), $args[0]);
+        $wp_customize->add_control( new $args[2]($wp_customize, $this->optionSlug($name), $args[1]));
+      }
+    }
+  }
+
+  public function headerOrFeaturedImage($imageSize = 'header-image'){
+    if(get_the_post_thumbnail(null, $size = $imageSize)){
+      $attachmentImageSrc = wp_get_attachment_image_src(get_post_thumbnail_id(), $imageSize);
+      return $attachmentImageSrc[0];
+    }else{
+      return get_header_image();
+    }
+
+    return "";
+  }
+
+  public function sanitizeHexColor($color) {
+    if('' === $color)
+			return '';
+
+		if (preg_match('|^#([A-Fa-f0-9]{3}){1,2}$|', $color))
+			return $color;
+
+    return null;
+	}
+
+  // PRIVATE FUNCTIONS
+
+  private function optionName($name){
+    return $this->themeSlug . "_options[" . $name . "]";
+  }
+
+  private function optionSlug($name){
+    return $this->themeSlug . "_" . $name;
+  }
+}
+?>
